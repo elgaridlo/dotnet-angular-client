dc5641fc2c9f1db5357cfd2a41733113e31f6bc3
Elga <elgaridlosinatriya@gmail.com>
2021-04-18 07:53:07 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  user: any;
+  
+
+  constructor(
+    private httpClient : HttpClient
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser() {
+    this.httpClient.get('https://localhost:5001/api/users').subscribe(
+      res => {
+        this.user = res;
+        console.log('uadsdsa = ', this.user);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 }
